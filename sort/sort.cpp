e2805456cfaab7a54755a0737af57eaae22ece9f
dmitry <kondra2lp@gmail.com>
2011-04-06 17:59:29 +0400

    deprecated

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -6,75 +6,108 @@
 
 using namespace std;
 
+typedef bool (*Metric)(double, double, int, int, double);
+typedef double (*Func)(double x); 
+
 class Function
 {
-    static const double EPS = 1e-8;
-
     int num;
+    static int count;
+    static double eps;
+
+    Func func;
 public:
     static double y;
-    static int metric;
+    static Metric metric;
 
-    class WrongFunction {};
-
-    Function(int num) : num(num) {
-        if (num > 10 || num < 1)
-            throw WrongFunction();
+    Function(Func func) : func(func) {
+        num = count++;
     }
 
-    double func(double x) const {
-        switch (num) {
-            case 1: return sin(x);
-            case 2: return cos(x);
-            case 3: return x * x;
-            case 4: return fabs(x);
-            case 5: return exp(x);
-            case 6: return sqrt(42 + x * x);
-            case 7: return x * x * x;
-            case 8: return cos(x + M_PI / 3);
-            case 9: return sin(x - M_PI / 42);
-            case 10: return log(2 + x * x);
-        }
-        return 0;
+    double calc_func(double x) const {
+        return func(x);
     }
 
     bool operator< (const Function& f) const {
-        if (metric == 1) {
-            if (func(y) < f.func(y))
-                return true;
-            if (fabs(func(y) - f.func(y)) < EPS && num < f.num)
-                return true;
-        } else if (metric == 2) {
-            if (fabs(func(y)) < fabs(f.func(y)) && num > f.num)
-                return true;
-            if (fabs(func(y) - f.func(y)) < EPS)
-                return true;
-        }
-        return false;
+        return metric(func(y), f.func(y), num, f.num, eps);
     }
 };
 
+bool metric1(double x, double y, int n, int m, double eps) {
+    if (x < y)
+        return true;
+    if (fabs(x - y) < eps && n < m)
+        return true;
+    return false;
+}
+
+
+bool metric2(double x, double y, int n, int m, double eps) {
+    if (fabs(x) < fabs(y))
+        return true;
+    if (fabs(x - y) < eps && n > m)
+        return true;
+    return false;
+}
+
+double sqr(double x) {
+    return x * x;
+}
+
+double mysqrt(double x) {
+    return sqrt(42 + x * x);
+}
+
+double cube(double x) {
+    return x * x * x;
+}
+
+double mycos(double x) {
+    return cos(x + M_PI / 3);
+}
+
+double mysin(double x) {
+    return sin(x - M_PI / 42);
+}
+
+double mylog(double x) {
+    return log(2 + x * x);
+}
+
 double Function::y;
-int Function::metric;
+double Function::eps = 1e-8;
+Metric Function::metric;
+int Function::count = 1;
 
 int main()
 {
-    const int SIZE = 10;
-
+    int k;
     double z;
 
-    cin >> Function::metric >> Function::y >> z;
+    cin >> k >> Function::y >> z;
+
+    if (k == 1)
+        Function::metric = metric1;
+    else if (k == 2)
+        Function::metric = metric2;
 
     vector<Function> arr;
 
-    for (int i = 1; i <= SIZE; i++) {
-        arr.push_back(Function(i));
-    }
+    arr.push_back(Function(sin));
+    arr.push_back(Function(cos));
+    arr.push_back(Function(sqr));
+    arr.push_back(Function(fabs));
+    arr.push_back(Function(exp));
+    arr.push_back(Function(mysqrt));
+    arr.push_back(Function(cube));
+    arr.push_back(Function(mycos));
+    arr.push_back(Function(mysin));
+    arr.push_back(Function(mylog));
 
     sort(arr.begin(), arr.end());
 
     for (int i = arr.size() - 1; i >= 0; i--) {
-        z = arr[i].func(z);
+        z = arr[i].calc_func(z);
     }
 
     cout << setprecision(10) << z << endl;
